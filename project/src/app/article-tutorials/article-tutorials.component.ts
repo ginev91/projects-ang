fba7eb1ede8d693ea5a36279087e34f25c525c13
ginev91 <48861476+ginev91@users.noreply.github.com>
2020-12-05 21:59:29 +0300

    Add files via upload

---
+++ project/src/app/article-tutorials/article-tutorials.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-article-tutorials',
+  templateUrl: './article-tutorials.component.html',
+  styleUrls: ['./article-tutorials.component.css']
+})
+export class ArticleTutorialsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
